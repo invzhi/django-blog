2d01e4e22ddc5f51a67c8118bfb646da63c78512
invzhi <invzhi@protonmail.com>
2017-07-07 10:07:52 +0800

    Change urlconf

--- mysite/article/urls.py
+++ mysite/article/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 app_name = 'article'
 urlpatterns = [
+    url(r'^$', views.index),
     url(r'^articles/$', views.index, name='index'),
     url(r'^article/(?P<article_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^search/$', views.search, name='search'),
