1016929c655017cb53ee2113b3d2b8c8074ffac5
invzhi <invzhi@protonmail.com>
2017-07-11 10:47:35 +0800

    Add markdown extension 'Abbreviations'

--- mysite/article/templatetags/markdown2html.py
+++ mysite/article/templatetags/markdown2html.py
@@ -8,7 +8,8 @@ register = template.Library()
 @register.filter
 def markdown2html(markdown_text):
     html = markdown.markdown(markdown_text, output_format='html5',
-                             extensions=['markdown.extensions.fenced_code',
+                             extensions=['markdown.extensions.abbr',
+                                         'markdown.extensions.fenced_code',
                                          'markdown.extensions.codehilite',
                                          'markdown.extensions.toc',
                                          ]
