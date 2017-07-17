9b3d6ce1c87339587752eef31ab724e44556e06c
invzhi <invzhi@protonmail.com>
2017-07-17 11:29:17 +0800

    Change url name

--- mysite/article/urls.py
+++ mysite/article/urls.py
@@ -5,9 +5,9 @@ from .views import ArticleListView, ArticleDetailView, TagArticleListView, Searc
 app_name = 'article'
 urlpatterns = [
     url(r'^$', ArticleListView.as_view()),  # TODO: beautiful index
-    url(r'^articles/$', ArticleListView.as_view(), name='index'),
-    url(r'^articles/([\w-]+)/$', TagArticleListView.as_view()),
-    url(r'^article/(?P<pk>[0-9]+)/$', ArticleDetailView.as_view(), name='detail'),
+    url(r'^articles/$', ArticleListView.as_view(), name='article-list'),
+    url(r'^articles/([\w-]+)/$', TagArticleListView.as_view(), name='tag-article-list'),
+    url(r'^article/(?P<pk>[0-9]+)/$', ArticleDetailView.as_view(), name='article-detail'),
     url(r'^search/$', SearchView.as_view(), name='search'),
     url(r'^about/$', AboutView.as_view(), name='about'),
 ]
