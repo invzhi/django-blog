a371c9326fab61c32637e756ab32445aae939d95
invzhi <invzhi@protonmail.com>
2017-08-03 19:38:49 +0800

    Add test

--- article/tests.py
+++ article/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Article
 
-# Create your tests here.
+
+class ArticleModelTests(TestCase):
+
+    def setUp(self):
+        Article.objects.create(title='article1',
+                               content='Hello Django\nThis is a article for django development.')
+
+    def test_article_summary(self):
+        article = Article.objects.get(title='article1')
+        self.assertEqual(article.summary(), 'Hello Django')
