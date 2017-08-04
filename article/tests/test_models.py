b6631889fb1156ef16da283050606d439b7a6b31
invzhi <invzhi@protonmail.com>
2017-08-04 19:12:56 +0800

    Add test for models and views

---
+++ article/tests/test_models.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+from ..models import Article, Tag
+
+
+class TagTest(TestCase):
+    def setUp(self):
+        Tag.objects.create(name='tag1')
+
+    def test_get_absolute_url(self):
+        tag = Tag.objects.get(name='tag1')
+        self.assertEqual(tag.get_absolute_url(), '/articles/tagged/%s/' % tag.name)
+
+
+class ArticleTest(TestCase):
+    def setUp(self):
+        Article.objects.create(title='article1',
+                               content='Hello Django\nThis is a article for django development.')
+
+    def test_get_absolute_url(self):
+        article = Article.objects.get(title='article1')
+        self.assertEqual(article.get_absolute_url(), '/articles/%s/' % article.id)
+
+    def test_article_summary(self):
+        article = Article.objects.get(title='article1')
+        self.assertEqual(article.summary(), 'Hello Django')
---
+++ article/tests/test_views.py
@@ -0,0 +1,43 @@
+from random import random
+from django.shortcuts import reverse
+from django.test import TestCase
+from ..models import Article, Tag
+
+
+class ViewTest(TestCase):
+    def setUp(self):
+        number_of_tags = 10
+        number_of_articles = 50
+        for tag_num in range(number_of_tags):
+            Tag.objects.create(name='tag%s' % tag_num)
+        self.tags = Tag.objects.all()
+        for article_num in range(number_of_articles):
+            article = Article.objects.create(title='Article %s' % article_num,
+                                             content='Content %s\nThis is a article for django test.' % article_num)
+            random_tags = []
+            for tag in self.tags:
+                if random() < 0.5:
+                    random_tags.append(tag)
+            article.tags.add(*random_tags)
+
+    def test_article_list_view(self):
+        resp = self.client.get(reverse('article:article-list'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_tagged_article_list_view(self):
+        for tag in self.tags:
+            resp = self.client.get(reverse('article:tagged-article-list', args=[tag]))
+            self.assertEqual(resp.status_code, 200)
+
+    def test_article_detail_view(self):
+        for article in Article.objects.all():
+            resp = self.client.get(reverse('article:article-detail', args=[article.id]))
+            self.assertEqual(resp.status_code, 200)
+
+    def test_search_view(self):
+        resp = self.client.get(reverse('article:search'))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_about_view(self):
+        resp = self.client.get(reverse('article:about'))
+        self.assertEqual(resp.status_code, 200)
