60b56d6ebc66ec5a6986cfe4a9241db78fe7203e
invzhi <invzhi@protonmail.com>
2017-08-09 21:03:03 +0800

    Change static folder

--- mysite/settings/common.py
+++ mysite/settings/common.py
@@ -86,6 +86,8 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, '')
