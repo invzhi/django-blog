6c14a4d469507613cdd5fa38bc622b79e7f6410e
invzhi <invzhi@protonmail.com>
2017-08-20 09:26:33 +0800

    Rename ArticleConfig to ArticlesConfig

--- articles/apps.py
+++ articles/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class ArticleConfig(AppConfig):
+class ArticlesConfig(AppConfig):
     name = 'articles'
--- invzhi/settings.py
+++ invzhi/settings.py
@@ -22,7 +22,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'articles.apps.ArticleConfig',
+    'articles.apps.ArticlesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
